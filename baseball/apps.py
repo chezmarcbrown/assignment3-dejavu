ef883102c56cddbb7c56821b6daf0e660e52c27c
chezmarcbrown <marc@chezbrown.org>
2022-09-27 12:42:25 -0800

    removed defualt "baseball"

--- baseball/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BaseballConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'baseball'
--- baseball/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "baseball"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("standings", views.standings, name="standings"),
-    path("advance-day", views.advance_date, name="advance-date"),
-]
